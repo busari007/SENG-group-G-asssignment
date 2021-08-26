9ddbbd81aca45afeb1fa958fd4a56fef8fc47475
busari007 <88407563+busari007@users.noreply.github.com>
2021-08-26 13:32:34 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,8 @@
+function prob(){
+    let x = prompt("Anything Else?");
+    if(x=="no"||"No"||"NO"){
+        alert("Then Have A Wonderful Browsing Experience");
+    }else{
+        alert("Kindly share it with us")
+    }
+}
